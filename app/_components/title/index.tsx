44dfb7da53f0ca04bfe5970a8c6ccad61ad58e6f
Roe Greene <roethedev@gmail.com>
2025-04-07 21:12:49 -0600

    finish setting up stylelint

--- app/_components/title/index.tsx
+++ app/_components/title/index.tsx
@@ -1,8 +1,9 @@
-import classNames from 'classnames'
-const Title: React.FC<{title: string, className?: string}> = ({title, className}) => {
-  return (
-      <h1 className={classNames('text-title', className)}>{title}</h1>
-  );
-}
+import classNames from "classnames";
+const Title: React.FC<{ title: string; className?: string }> = ({
+  title,
+  className,
+}) => {
+  return <h1 className={classNames("text-title", className)}>{title}</h1>;
+};
 
 export default Title;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,56 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import jest from "eslint-plugin-jest";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default defineConfig([
+  globalIgnores(["public/*.loader.js"]),
+  {
+    extends: compat.extends(
+      "eslint:recommended",
+      "prettier",
+      "next/core-web-vitals",
+      "next/typescript",
+    ),
 
-export default eslintConfig;
+    plugins: {
+      prettier,
+    },
+
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+        },
+      ],
+    },
+  },
+  {
+    files: ["app/**/test.tsx"],
+
+    plugins: {
+      jest,
+    },
+
+    languageOptions: {
+      globals: {
+        ...jest.environments.globals.globals,
+      },
+    },
+
+    rules: {},
+  },
+]);
