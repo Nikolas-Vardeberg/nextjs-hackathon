a6d30f5b0307140516a3577cf3186a73853cf6f8
niklusrook <nikolas.vardeberg1@gmail.com>
2025-04-08 13:11:42 +0200

    feat: cookieconsent script

--- app/_components/title/index.tsx
+++
@@ -1,9 +0,0 @@
-import classNames from "classnames";
-const Title: React.FC<{ title: string; className?: string }> = ({
-  title,
-  className,
-}) => {
-  return <h1 className={classNames("text-title", className)}>{title}</h1>;
-};
-
-export default Title;
