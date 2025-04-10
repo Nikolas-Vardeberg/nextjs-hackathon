08a5478555d407753b05b45fe13701f4213aeef7
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-04-10 23:33:43 +0200

    feat: steps bar

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,4 +1,5 @@
 import ButtonHandler from "@/common/components/sign-up/button-handler";
+import HightlightBar from "@/common/components/sign-up/highlight-bar";
 import RegistrationStep from "@/common/components/sign-up/registration-step";
 import SignUpFormProvider from "@/common/providers/form-provider";
 
@@ -10,6 +11,7 @@ const Page = () => {
                 <div className="flex flex-col gap-3">
                     <RegistrationStep />
                     <ButtonHandler />
+                    <HightlightBar />
                 </div>
             </SignUpFormProvider>
         </div>
