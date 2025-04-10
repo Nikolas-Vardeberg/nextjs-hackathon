e897b3837500ecddfcb029141cca7ed6c3752851
Roe Greene <roethedev@gmail.com>
2025-04-09 18:34:40 -0600

    fix lint errors

--- common/components/atoms/layouts/Container.tsx
+++ common/components/atoms/layouts/Container.tsx
@@ -3,13 +3,14 @@ import clsx from "clsx";
 type ContainerProps = {
   children: React.ReactNode;
   className?: string;
-}
+};
 
-const Container: React.FC<ContainerProps> = ({ children, className }: ContainerProps) => {
+const Container: React.FC<ContainerProps> = ({
+  children,
+  className,
+}: ContainerProps) => {
   return (
-    <div className={clsx("container mx-auto px-8", className)}>
-      {children}
-    </div>
+    <div className={clsx("container mx-auto px-8", className)}>{children}</div>
   );
 };
 
--- common/components/atoms/layouts/Grid.tsx
+++ common/components/atoms/layouts/Grid.tsx
@@ -1,36 +1,36 @@
-import { css, styled } from 'styled-components';
+import { css, styled } from "styled-components";
 
 export interface SizeProps {
-	sm?: number;
-    md?: number;
-	lg?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
 }
 
 interface GridProps {
-	columns?: SizeProps;
-	gap?: SizeProps;
-	x?: SizeProps;
-	y?: SizeProps;
-	container?: boolean;
-	rowNotAuto?: boolean;
+  columns?: SizeProps;
+  gap?: SizeProps;
+  x?: SizeProps;
+  y?: SizeProps;
+  container?: boolean;
+  rowNotAuto?: boolean;
 }
 
 interface GridItemProps {
-	colSpan?: SizeProps;
-	colStart?: SizeProps;
-	card?: boolean;
+  colSpan?: SizeProps;
+  colStart?: SizeProps;
+  card?: boolean;
 }
 
-const defaultSizeProps: SizeProps = { sm: 4, lg: 12 }; 
-const defaultGap: SizeProps = { sm: 16, lg: 28 }; 
+const defaultSizeProps: SizeProps = { sm: 4, lg: 12 };
+const defaultGap: SizeProps = { sm: 16, lg: 28 };
 
-const containerPaddings: SizeProps = { sm: 16, lg: 38 }; 
+const containerPaddings: SizeProps = { sm: 16, lg: 38 };
 
 const generateGridTemplateColumns = (columns: SizeProps = defaultSizeProps) => {
-const col = { ...defaultSizeProps, ...columns }; 
-	return css`
+  const col = { ...defaultSizeProps, ...columns };
+  return css`
     grid-template-columns: repeat(${col.sm}, 1fr);
-    
+
     @media (min-width: 1024px) {
       grid-template-columns: repeat(${col.lg}, 1fr);
     }
@@ -38,12 +38,13 @@ const col = { ...defaultSizeProps, ...columns };
 };
 
 export const Grid = styled.div
-	.attrs((p) => ({
-		className: p.className || 'grid',
-	}))
-	.withConfig({
-		shouldForwardProp: (prop) => !['columns', 'gap', 'x', 'y', 'container', 'rowNotAuto'].includes(prop),
-	})<GridProps>`
+  .attrs((p) => ({
+    className: p.className || "grid",
+  }))
+  .withConfig({
+    shouldForwardProp: (prop) =>
+      !["columns", "gap", "x", "y", "container", "rowNotAuto"].includes(prop),
+  })<GridProps>`
     display: grid;
     width: 100%;
     ${({ container }) => container && `padding: 0 ${containerPaddings.sm}px; margin-left: auto; margin-right: auto;`};
@@ -51,7 +52,7 @@ export const Grid = styled.div
     column-gap: ${({ x = defaultGap }) => x.sm}px; // Default x gap
     row-gap: ${({ y = defaultGap }) => y.sm}px; // Default y gap
     grid-template-columns: repeat(${(props) => props.columns?.sm || defaultSizeProps.sm}, 1fr); // Default columns
-    ${({ rowNotAuto }) => !rowNotAuto && ' grid-auto-rows: 1fr;'}
+    ${({ rowNotAuto }) => !rowNotAuto && " grid-auto-rows: 1fr;"}
 
     ${(props) => generateGridTemplateColumns(props.columns)}
 
@@ -73,19 +74,19 @@ export const Grid = styled.div
 `;
 
 const generateGridColumn = (colSpan?: SizeProps, colStart?: SizeProps) => {
-	const defaultSpan = { sm: 4, lg: 12 };
-	const span = {
-		...defaultSpan,
-		sm: colSpan?.sm || defaultSpan.sm,
-		lg: colSpan?.lg || colSpan?.sm || defaultSpan.lg,
-	};
-	const start = {
-		...{ sm: 1, lg: 1 },
-		sm: colStart?.sm || 1,
-		lg: colStart?.lg || colStart?.sm || 1,
-	};
+  const defaultSpan = { sm: 4, lg: 12 };
+  const span = {
+    ...defaultSpan,
+    sm: colSpan?.sm || defaultSpan.sm,
+    lg: colSpan?.lg || colSpan?.sm || defaultSpan.lg,
+  };
+  const start = {
+    ...{ sm: 1, lg: 1 },
+    sm: colStart?.sm || 1,
+    lg: colStart?.lg || colStart?.sm || 1,
+  };
 
-	return css`
+  return css`
     display: inline-grid;
     grid-column: ${start.sm} / span ${span.sm};
 
@@ -96,7 +97,7 @@ const generateGridColumn = (colSpan?: SizeProps, colStart?: SizeProps) => {
 };
 
 export const GridItem = styled.div.withConfig({
-	shouldForwardProp: (prop) => !['colSpan', 'colStart', 'card'].includes(prop),
+  shouldForwardProp: (prop) => !["colSpan", "colStart", "card"].includes(prop),
 })<GridItemProps>`
   ${(props) => generateGridColumn(props.colSpan, props.colStart)}
 `;
