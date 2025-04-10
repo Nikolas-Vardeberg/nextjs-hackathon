dac8f08f4bf3d22b704ba2dc219612a046776b09
Roe Greene <roethedev@gmail.com>
2025-04-09 20:24:32 -0600

    fix interface type

--- lib/mongoose/models/types.ts
+++ lib/mongoose/models/types.ts
@@ -19,7 +19,7 @@ export interface OpenAIDocument {
   createdAt: Date;
   updatedAt: Date;
   userDocID: ObjectId;
-  openAIID: number; // an ID is generated per conversation with open ai
+  openAIID: string; // an ID is generated per conversation with open ai
   openAIDataCache?: string; // save the data from open ai here so if it goes down we can still use it
   /* The custom values are here so you dont need to change the model - use these for whatever you want */
   customValue_1?: string;
