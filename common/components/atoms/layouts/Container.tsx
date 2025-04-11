e1a175ef0f2008223d6f86d3649d2dd45cd20390
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-11 04:07:19 -0400

    feat: add `as` prop to Container to support semantic elements

--- common/components/atoms/layouts/Container.tsx
+++ common/components/atoms/layouts/Container.tsx
@@ -3,16 +3,20 @@ import { cn } from "@/lib/cn";
 type ContainerProps = {
   children: React.ReactNode;
   className?: string;
+  as?: "div" | "section" | "article" | "main" | "aside" | "header" | "footer";
 };
 
 const Container: React.FC<ContainerProps> = ({
   children,
   className,
+  as: Component = "div",
 }: ContainerProps) => {
   return (
-    <div className={cn("container mx-auto px-4 sm:px-6 md:px-8", className)}>
+    <Component
+      className={cn("container mx-auto px-4 sm:px-6 md:px-8", className)}
+    >
       {children}
-    </div>
+    </Component>
   );
 };
 
