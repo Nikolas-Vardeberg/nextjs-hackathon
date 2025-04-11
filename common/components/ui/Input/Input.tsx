ea797bcc009dffdb623457fd13ff556a29bac0b3
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-11 03:30:15 -0400

    chore: add tailwind-merge dependency and refactor Input component class handling

--- common/components/ui/Input/Input.tsx
+++ common/components/ui/Input/Input.tsx
@@ -1,11 +1,11 @@
-import clsx from "clsx";
+import { cn } from "@/lib/cn";
 import { InputProps } from "./types";
 
 export default function Input({ className, ...props }: InputProps) {
   return (
     <input
       data-testid="input"
-      className={clsx(
+      className={cn(
         "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-black disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className,
       )}
