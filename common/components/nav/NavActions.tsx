ae584a9d27c09b99fcf3e54931f5e66ca4bf9e6b
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-12 16:57:20 -0400

    feat: enhance NavActions to close on user action

--- common/components/nav/NavActions.tsx
+++ common/components/nav/NavActions.tsx
@@ -4,36 +4,61 @@ import Link from "next/link";
 import Button from "@/common/components/ui/Button";
 import { useUser, useClerk } from "@clerk/nextjs";
 
-function UserActions() {
+function UserActions({ onClose }: NavActionsProps) {
   const { signOut } = useClerk();
+
+  const handleSignOut = () => {
+    signOut();
+    onClose?.();
+  };
+
+  const handleDashboard = () => {
+    onClose?.();
+  };
 
   return (
     <>
-      <Button variant="inverted-tealwave" onClick={() => signOut()}>
+      <Button variant="inverted-tealwave" onClick={handleSignOut}>
         Sign Out
       </Button>
-      <Button variant="tealwave" asChild>
+      <Button variant="tealwave" onClick={handleDashboard} asChild>
         <Link href="/dashboard">Dashboard</Link>
       </Button>
     </>
   );
 }
 
-function GuestActions() {
+function GuestActions({ onClose }: NavActionsProps) {
+  const handleSignIn = () => {
+    onClose?.();
+  };
+
+  const handleSignUp = () => {
+    onClose?.();
+  };
+
   return (
     <>
-      <Button variant="inverted-tealwave" asChild>
+      <Button variant="inverted-tealwave" onClick={handleSignIn} asChild>
         <Link href="/sign-in">Sign In</Link>
       </Button>
-      <Button variant="tealwave" asChild>
+      <Button variant="tealwave" onClick={handleSignUp} asChild>
         <Link href="/sign-up">Get Started</Link>
       </Button>
     </>
   );
 }
 
-export default function NavActions() {
+type NavActionsProps = {
+  onClose?: () => void;
+};
+
+export default function NavActions({ onClose }: NavActionsProps) {
   const { user } = useUser();
 
-  return user ? <UserActions /> : <GuestActions />;
+  return user ? (
+    <UserActions onClose={onClose} />
+  ) : (
+    <GuestActions onClose={onClose} />
+  );
 }
