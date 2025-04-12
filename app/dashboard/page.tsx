cc1b4a3d9654ad26caf8e01e267a21a2290e4fd9
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-12 03:42:10 -0400

    feat: implement redirect in DashboardPage if no auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   return <div>Dashboard Page</div>;
 }
