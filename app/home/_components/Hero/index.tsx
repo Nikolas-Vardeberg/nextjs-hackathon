9e1fa8546b586fe6eac063fd5900a9e010d0b073
Roe Greene <roethedev@gmail.com>
2025-04-12 15:53:52 -0600

    cleanup home page component

---
+++ app/home/_components/Hero/index.tsx
@@ -0,0 +1,27 @@
+import Hero from "@/common/components/hero/Hero";
+import { HeroContent } from "@/common/components/hero/HeroContent";
+import { HeroSearch } from "@/common/components/hero/HeroSearch";
+import useSearch from "@/common/hooks/use-search";
+import heroBg from "@/assets/images/hero/background.webp";
+
+const HeroComponent: React.FC = () => {
+  const { handleSearch, searchPlaceholder, buttonTextState } = useSearch();
+
+  return (
+    <Hero backgroundImage={heroBg.src}>
+      <HeroContent
+        title="Your Vacation Starts Here"
+        description="Tell us what inspires you, and our AI will craft the perfect journey tailored just for you"
+        alignment="left"
+      >
+        <HeroSearch
+          placeholder={searchPlaceholder}
+          buttonText={buttonTextState}
+          onSearch={handleSearch}
+        />
+      </HeroContent>
+    </Hero>
+  );
+};
+
+export default HeroComponent;
