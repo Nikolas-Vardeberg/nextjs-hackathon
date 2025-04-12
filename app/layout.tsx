665c545f054fec7ee0816724f0010230d7cbbb14
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-12 03:22:54 -0400

    style: force light mode until we work on dark mode styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default async function RootLayout({
             attribute="class"
             defaultTheme="system"
             enableSystem
+            forcedTheme="light"
             disableTransitionOnChange
           >
             <Navbar />
