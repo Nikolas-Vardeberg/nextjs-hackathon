195dfacc4cbc8d65d2ed908b41c0399f0f693bf7
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-13 17:01:14 -0400

    fix: ran lint

--- common/components/vacation-search-modal/index.tsx
+++ common/components/vacation-search-modal/index.tsx
@@ -77,7 +77,11 @@ export default function VacationSearchModal({
     if (currentStep < VACATION_SEARCH_QUESTIONS.length - 1) {
       setCurrentStep((prevStep) => prevStep + 1);
     } else {
-      const finalAnswers = [...answers.slice(0, currentStep), data.answer, ...answers.slice(currentStep + 1)];
+      const finalAnswers = [
+        ...answers.slice(0, currentStep),
+        data.answer,
+        ...answers.slice(currentStep + 1),
+      ];
       onSearch(finalAnswers);
       onClose();
     }
