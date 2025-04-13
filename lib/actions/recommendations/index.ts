69d27e520fd907acc2ad6ab7328e69bf9fce9122
Roe Greene <roethedev@gmail.com>
2025-04-12 21:32:12 -0600

    cleanup search recommendations function

--- lib/actions/recommendations/index.ts
+++ lib/actions/recommendations/index.ts
@@ -1,21 +1,10 @@
 "use server";
-import { auth } from "@clerk/nextjs/server";
 import { fetchGoogleDetails, fetchOpenAIRecommendations } from "./engine";
 import { OpenAIResponse } from "./types";
 
 export async function getSearchRecommendations(answers: string[]) {
-  // Step 1: Retrieve session data
-  const { sessionId } = await auth();
-  if (!sessionId) {
-    throw new Error("TODO(GUEST): User is not authenticated");
-  }
-
-  // Step 2: Call OpenAI API to get recommendations
   const recommendations: OpenAIResponse =
     await fetchOpenAIRecommendations(answers);
 
-  // Step 3: Save the OpenAI snapshot for session continuity
-
-  // Step 4: Return recommendations
   return await fetchGoogleDetails(recommendations?.choices[0].message.content);
 }
