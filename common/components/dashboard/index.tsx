a5c981090d6c682edbd9800d65a55588a8e7905f
Roe Greene <roethedev@gmail.com>
2025-04-13 18:37:11 -0600

    layout the widdgets on the dashboard

--- common/components/dashboard/index.tsx
+++ common/components/dashboard/index.tsx
@@ -5,10 +5,15 @@ import SearchHistory from "./search-history";
 const Dashboard: React.FC = () => {
   return (
     <div>
-      Dashboard goes here
-      <ActivityPanel />
-      <SavedPanel />
-      <SearchHistory />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-8">
+        <div className="lg:col-span-2">
+          <SearchHistory />
+        </div>
+        <div>
+          <ActivityPanel />
+          <SavedPanel />
+        </div>
+      </div>
     </div>
   );
 };
