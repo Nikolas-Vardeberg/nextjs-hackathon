9c96a7b175aac34f8495568e38906a6c736d7ae6
Nandor <n3ndor@gmail.com>
2025-04-15 13:20:45 -0300

    change grid to scroll view

---
+++ common/components/home/scroll-section/index.tsx
@@ -0,0 +1,25 @@
+import { RecommendationItem } from "@/lib/actions/recommendations/types";
+import PlaceCard from "../place-card";
+
+const ScrollSection: React.FC<{
+  recommendations?: RecommendationItem[];
+  title?: string;
+}> = ({ recommendations, title }) => {
+  return (
+    <>
+      {title && <h1 className="text-2xl font-bold capitalize mb-4">{title}</h1>}
+      <div className="w-full flex overflow-x-auto snap-x snap-mandatory gap-4 pb-4">
+        {recommendations?.map((rec, index) => (
+          <div
+            key={rec.business_name || index}
+            className="snap-start min-w-[280px]"
+          >
+            <PlaceCard rec={rec} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ScrollSection;
