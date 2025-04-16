c0ffbc3a5f79bf8101265b269e8290b356759cd3
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-16 18:44:14 -0400

    style: simplify layout and spacing in SearchHeader component

--- common/components/dashboard/search-history/search-header/index.tsx
+++ common/components/dashboard/search-history/search-header/index.tsx
@@ -3,15 +3,13 @@ import React from "react";
 const SearchHeader: React.FC = () => {
   return (
     <div className="flex flex-col space-y-1.5 p-6 pb-3">
-      <div className="flex items-center justify-between">
-        <div>
-          <h3 className="text-2xl font-semibold leading-none tracking-tight">
-            Search History
-          </h3>
-          <p className="text-sm text-muted-foreground">
-            Your recent vacation and rental searches and results
-          </p>
-        </div>
+      <div className="flex flex-col gap-1 justify-between">
+        <h3 className="text-2xl font-semibold leading-none tracking-tight">
+          Search History
+        </h3>
+        <p className="text-sm text-muted-foreground">
+          Your recent vacation and rental searches and results
+        </p>
       </div>
     </div>
   );
