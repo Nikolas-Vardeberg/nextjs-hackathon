14bff68adb69fa0afe5a31e1171ebda4a8ef41cf
Lucas Winkler <lucaswinkler@gmail.com>
2025-04-15 22:50:24 -0400

    refactor: conditionally render navigation links in DesktopNav and MobileNav components

--- common/constants/nav-links.ts
+++ common/constants/nav-links.ts
@@ -1,14 +1,6 @@
 export const NAV_LINKS = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Test 1",
-    href: "/test-1",
-  },
-  {
-    label: "Test 2",
-    href: "/test-2",
-  },
+  // {
+  //   label: "Home",
+  //   href: "/",
+  // },
 ];
